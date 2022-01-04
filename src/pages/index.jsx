f48b8e116453ee141eb054c2b03dee4bc9735b4a
uglo1 <yujiro.takahashi0912@gmail.com>
2022-01-04 20:34:46 +0900

    ✨ useEffectの実験

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 
 export default function Home() {
   const foo = 1;
@@ -15,6 +15,15 @@ export default function Home() {
       alert(foo);
     },[],
   );
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue"
+
+    return () => {
+      document.body.style.backgroundColor = ""
+    }
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>

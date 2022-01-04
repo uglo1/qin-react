9ed363d1f9445caaa62c7941b1fd8e06a21b2535
uglo1 <yujiro.takahashi0912@gmail.com>
2022-01-04 13:42:30 +0900

    🏗️ 拡張子・ディレクトリ構成・インポート方法の変更・_appを用いた処理共通化

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,13 +1,11 @@
-import classes from './Main.module.css'
-import { Headline } from '../components/Headline'
-import { Links } from './Links'
+import classes from 'src/components/Main/Main.module.css'
+import { Headline } from 'src/components/Headline'
+import { Links } from 'src/components/Links'
 
 export function Main(props) {
   return (
     <main className={classes.main}>
-      <Headline 
-        page={props.page}
-      >
+      <Headline page={props.page}>
         <code className={classes.code}>pages/{props.page}.js</code>
       </Headline>
       <Links />
---
+++ src/pages/about.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import { Footer } from 'src/components/Footer'
+import { Header } from 'src/components/Header'
+import { Main } from 'src/components/Main'
+import styles from 'src/styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>About Page</title>
+      </Head>
+      <Header />
+      <Main page="about" />
+      <Footer />
+    </div>
+  )
+}

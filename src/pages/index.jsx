f8dacfdefd5ffd7918b390ed403f4795db8345cf
uglo1 <yujiro.takahashi0912@gmail.com>
2022-01-04 23:02:05 +0900

    ⚡️ useCallbackとuseEffectの第二引数

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,16 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount(count => count + 1);
-    setCount(count => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if(count < 10) {
+      setCount(count => count + 1);
+    }
+  },[count],)
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue"
@@ -19,7 +20,7 @@ export default function Home() {
     return () => {
       document.body.style.backgroundColor = ""
     }
-  }, [])
+  }, [count])
 
   return (
     <div className={styles.container}>

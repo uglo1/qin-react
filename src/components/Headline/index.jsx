00f079b619f4f38ea4386341669e9aa6a8229a3b
uglo1 <yujiro.takahashi0912@gmail.com>
2022-01-10 18:01:04 +0900

    ✨ コンポーネント間のステートのリフトアップ

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -8,7 +8,7 @@ export function Headline(props) {
       </h1>
 
       <p className={classes.description}>
-        Get started by editing {props.children}
+        アイテムの数は {props.children} 個です。
       </p>
     </>
   )

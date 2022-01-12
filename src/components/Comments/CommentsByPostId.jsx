e8015ff255b4b5e684ba84a0d7fdb56cb0c22b57
uglo1 <yujiro.takahashi0912@gmail.com>
2022-01-12 16:41:15 +0900

    ✨ 投稿の詳細ページにコメントを表示

---
+++ src/components/Comments/CommentsByPostId.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { useCommentsByPostsId } from "src/hooks/useFetchArray";
+
+export const CommentsByPostId = (props) => {
+  const { data, error, isLoading, isEmpty } = useCommentsByPostsId(props.id);
+
+  if (isLoading) {
+    return <div>ローディング中</div>;
+  }
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (isEmpty) {
+    return <div>データは空です。</div>;
+  }
+
+  return (
+    <ol>
+      {data.map((comment) => {
+        return (
+          <li key={comment.id}>
+            <Link href={`/comments/${comment.id}`}>
+              <a>{comment.body}</a>
+            </Link>
+          </li>
+        );
+      })}
+    </ol>
+  );
+};
--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -2,32 +2,30 @@ import { fetcher } from "src/utils/fetcher";
 import useSWR from "swr";
 
 export const useFetchArray = (url) => {
-  const { data, error } = useSWR(
-    url, 
-    fetcher
-  );
+  const { data, error } = useSWR(url, fetcher);
 
   return {
     data,
     error,
     isLoading: !error && !data,
     isEmpty: data && data.length === 0,
-  }
-}
+  };
+};
 
 const API_URL = "https://jsonplaceholder.typicode.com";
 
 export const usePosts = () => {
   return useFetchArray(`${API_URL}/posts`);
-}
+};
 
 export const useUsers = () => {
   return useFetchArray(`${API_URL}/users`);
-}
+};
 
 export const useComments = () => {
   return useFetchArray(`${API_URL}/comments`);
-}
+};
 
-
-
+export const useCommentsByPostsId = (id) => {
+  return useFetchArray(id ? `${API_URL}/comments?postId=${id}` : null);
+};

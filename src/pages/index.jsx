1634ef13ab40c367a03da7e222323cc11b41b07a
uglo1 <yujiro.takahashi0912@gmail.com>
2022-01-12 13:12:21 +0900

    Revert "🐛 パスの変更し忘れ修正"
    This reverts commit d25334c16bc6f68ec1db26bfe5ab109b488cfa62.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,35 @@
 import Head from 'next/head'
+import { Footer } from 'src/components/Footer'
 import { Header } from 'src/components/Header'
+import { Main } from 'src/components/Main'
 import styles from 'src/styles/Home.module.css'
 
-const Index = () => {
+const About = (props) => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Index Page</title>
+        <title>About Page</title>
       </Head>
       <Header />
-      <h1>JSONPlaceholderからデータ取得</h1>
+
+      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
+      <button onClick={props.handleClick}>ボタン</button>
+      <button onClick={props.handleDisplay}>{props.isShow ? "非表示" : "表示"}</button>
+
+      <button onClick={props.handleAdd}>追加</button>
+      <input type="text" value={props.text} onChange={props.handleChange}/>
+      <ul>
+        {props.array.map((item) => {
+          return(
+            <li key={item}>{item}</li>
+          )
+        })}
+      </ul>
+
+      <Main page="about" />
+      <Footer />
     </div>
   )
 }
 
-export default Index;
+export default About;

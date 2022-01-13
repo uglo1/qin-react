aaa045200a05f9e9f474e3134c1583fe75e4b490
uglo1 <yujiro.takahashi0912@gmail.com>
2022-01-13 14:50:32 +0900

    🎨 投稿取得周りのアーキテクチャの変更

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,10 +1,12 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { CommentsByPostId } from "src/components/Comments/CommentsByPostId";
 import { UserByUserId } from "src/components/User/UserByUserId";
 import { usePost } from "src/hooks/usePost";
 
 export const Post = () => {
-  const { data, error, isLoading } = usePost();
+  const router = useRouter();
+  const { data, error, isLoading } = usePost(router.query.id);
 
   if (isLoading) {
     return <div>ローディング中</div>;

b7f2b8bb41558a7718f8cbc7ec9a6aac09b77cbb
uglo1 <yujiro.takahashi0912@gmail.com>
2022-01-13 15:04:05 +0900

    🚚 複数形を単数形に変更

--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -14,22 +14,25 @@ export const useFetchArray = (url) => {
 
 const API_URL = "https://jsonplaceholder.typicode.com";
 
+// posts
 export const usePosts = () => {
   return useFetchArray(`${API_URL}/posts`);
 };
 
+export const usePostsByUserId = (id) => {
+  return useFetchArray(id ? `${API_URL}/posts?userId=${id}` : null);
+};
+
+// users
 export const useUsers = () => {
   return useFetchArray(`${API_URL}/users`);
 };
 
+// comments
 export const useComments = () => {
   return useFetchArray(`${API_URL}/comments`);
 };
 
-export const useCommentsByPostsId = (id) => {
+export const useCommentsByPostId = (id) => {
   return useFetchArray(id ? `${API_URL}/comments?postId=${id}` : null);
-};
-
-export const usePostsByUserId = (id) => {
-  return useFetchArray(id ? `${API_URL}/posts?userId=${id}` : null);
 };

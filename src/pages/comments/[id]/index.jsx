9604f1f5a3135d8385a9f516b6babdf622b529df
uglo1 <yujiro.takahashi0912@gmail.com>
2022-01-15 19:54:25 +0900

    ⚡️ コメントの個別ページをSG化

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,13 +1,45 @@
 import { Header } from "src/components/Header";
 import { Comment } from "src/components/Comment";
+import { SWRConfig } from "swr";
 
-const CommentsId = () => {
+export const getStaticPaths = async () => {
+  const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const commentsData = await comments.json();
+  const paths = commentsData.map((comment) => ({
+    params: { id: comment.id.toString() },
+  }));
+  return {
+    paths, // ここでStatic Generationするpathを定める。
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (ctx) => {
+  const { id } = ctx.params;
+  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+  const comments = await fetch(COMMENT_API_URL);
+  const commentsData = await comments.json();
+
+  return {
+    props: {
+      fallback: {
+        [COMMENT_API_URL]: commentsData,
+      },
+    },
+  };
+};
+
+const CommentsId = (props) => {
+  const { fallback } = props;
+
   return (
     <div>
-      <Header />
-      <Comment />
+      <SWRConfig value={{ fallback }}>
+        <Header />
+        <Comment />
+      </SWRConfig>
     </div>
   );
-}
+};
 
 export default CommentsId;
